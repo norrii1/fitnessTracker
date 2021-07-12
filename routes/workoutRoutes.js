9b28be42c9ebd5da4dec21ac4dceba8d94c5616f
norrii1 <alexnoriega47@gmail.com>
2021-07-12 14:54:52 -0700

    workout range

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -14,6 +14,13 @@ router.get('/workouts', (req, res) => Workout.aggregate([
     .then(workouts => res.json(workouts))
     .catch(err => console.log(err)))
 
+router.get('/workouts/range', (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err))
+})
+
 // GET one workout 
 router.get('/workouts/:id', (req, res) => Workout.findById(req.params.id)
   .populate('exercises')
